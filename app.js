74e5f259ad1cd918a4b6dfb52d2740e68bc76ca1
mihir <mihirgadwalkar@gmail.com>
2022-05-18 10:58:50 +0530

    minor modifications

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let weather = {
-    apiKey: "5bc2b4f40acfffa046713955a4370d52",
+    apiKey: "cbec63717ac2e45bb48470c0f358fdcb",
     fetchWeather: function (city) {
       fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=" +
@@ -21,7 +21,7 @@ let weather = {
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      console.log(name, icon, description, temp, humidity, speed);
+      //console.log(name, icon, description, temp, humidity, speed);
       document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + ".png";
